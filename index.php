52d119571efebd6df45c7575da8c9d779e3d7a90
AlvaroPlazaSanz <55554546+alvaroplazasanz@users.noreply.github.com>
2021-04-12 12:14:16 +0200

    Update login.php

--- index.php
+++ index.php
@@ -21,7 +21,10 @@
 			<img src="img/inicio.jpg" alt="Imagen principal pagina inicio">
 			<p> CALENDARIO ACTIVIDADES</p>
 			<img src="img/calendario-abril-2021.jpg" alt="Imagen calendario provisional">
-			
+			<p> ¿QUIÉNES SOMOS?
+            Hypefit es un innovador gimnasio online que incluye tanto recetas como rutinas equilibradas y pensadas para
+            los distintas necesidades de nuestro clientes</p>
+            <a href="quienesSomos.php">Ver más</a>
 		</div>
 		<?php
 			require("includes/comun/pie.php");
