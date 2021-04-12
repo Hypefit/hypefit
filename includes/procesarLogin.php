41f3695afb6f8bf20751e271b1ba456fbcb3ed40
AlvaroPlazaSanz <55554546+AlvaroPlazaSanz@users.noreply.github.com>
2021-04-12 11:56:53 +0200

    Create procesarLogin.php

---
+++ includes/procesarLogin.php
@@ -0,0 +1,62 @@
+<?php
+	session_start();
+	$username = htmlspecialchars(trim(strip_tags($_REQUEST["username"])));
+	$password = htmlspecialchars(trim(strip_tags($_REQUEST["password"])));
+	$email = htmlspecialchars(trim(strip_tags($_REQUEST["email"])));
+
+	$dao = new UsuarioDAO();
+	$usuario = $dao->getUsuarioPorEmail($username);
+
+	$hash = $usuario->getHashPassword();
+	
+	if ($username == "user" && $password == "userkey") {
+		$_SESSION["login"] = true;
+		$_SESSION["nombre"] = "Usuario";
+	}
+	
+	else if ($username == "admin" && $password == "adminkey") {
+		$_SESSION["login"] = true;
+		$_SESSION["nombre"] = "Administrador";
+		$_SESSION["esAdmin"] = true;
+	}
+?>
+
+<!-- <!DOCTYPE html> -->
+<html>
+ <head>
+  	<link rel="stylesheet" type="text/css" href="estilo.css" />
+	<meta charset="utf-8">
+	<title>Hypefit | Login</title>
+
+    <link rel="icon" 
+    type="image/png" 
+    href="img/favicon.png">
+</head>
+
+<body>
+<div id="contenedor">
+
+	<?php
+	require("cabecera.php");
+	?>
+
+<div id="contenido">
+	<?php
+		if (!isset($_SESSION["login"])) { //Usuario incorrecto
+			echo "<h1>ERROR</h1>";
+			echo "<p>El usuario o contraseña no son válidos.</p>";
+		}
+		else { //Usuario registrado
+			echo "<h1>Bienvenido {$_SESSION['nombre']}</h1>";
+			echo "<p>Usa el menú de Hypefit para navegar.</p>";
+		}
+	?>
+</div>
+
+    <?php
+	  #include("sidebarDer.php");
+	  #include("pie.php");
+	?>
+
+</div> <!-- Fin del contenedor -->
+</body></html>
