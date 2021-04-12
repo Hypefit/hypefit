59f41399ca6571d713221404b28f2f4f45e294cb
Gonzalo Martínez Berzal <60604451+WantonFrito@users.noreply.github.com>
2021-04-12 11:49:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
-<!DOCTYPE html>
-<html>
-	<head>
+<?php session_start(); ?>
+	<html lang="es"> 
+		<head>
 		<link rel="stylesheet" type="text/css" href="estilo.css" />
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>Portada</title>
+		<meta charset="utf-8">
+		<title>Hypefit | Login</title>
+
+		<link rel="icon" 
+		type="image/png" 
+		href="img/favicon.png">
 	</head>
 	
 	<body>
@@ -13,11 +17,14 @@
 		?>
 		
 		<div id="contenido">
-			<h1>Página principal</h1>
-			<p> Aquí está el contenido público, visible para todos los usuarios. </p>
+			<p>  Bienvenido a HYPEFIT</p>
+			<img src="img/inicio.png" alt="Imagen principal pagina inicio">
+			<p> CALENDARIO ACTIVIDADES</p>
+			<img src="img/calendario-abril-2021.jpg" alt="Imagen calendario provisional">
+			
 		</div>
-		<?php
-		require("pie.php");
+		# <?php
+		# require("pie.php");
 		?>
 </div>
 </body>
