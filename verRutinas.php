f2df95ebe12e10768043909c6a21f4b3269cfa0d
Gonzalo Martínez Berzal <60604451+WantonFrito@users.noreply.github.com>
2021-04-14 11:01:37 +0200

    Modificación fichero verRutinas.php

--- verRutinas.php
+++ verRutinas.php
@@ -1,1 +1,10 @@
 <?php
+
+require_once __DIR__ . '/includes/config.php';
+$tituloPagina = 'Hypefit | Rutinas';
+
+$contenidoPrincipal = "<div id="contenido">";
+$contenidoPrincipal .= mostrarRutina($_REQUEST["id"]);
+$contenidoPrincipal .= "</div>";
+
+require __DIR__ . '/includes/comun/layout.php';
