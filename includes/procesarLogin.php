f88ed6761fe3e1866645faa13f8953a43a2636c1
LlucBonet <mbonet03@ucm.es>
2021-04-14 12:09:46 +0200

    refactorizar

--- includes/procesarLogin.php
+++ includes/procesarLogin.php
@@ -5,23 +5,8 @@ require_once __DIR__.'/config.php';
 	$email = htmlspecialchars(trim(strip_tags($_REQUEST["email"])));
 	$password = htmlspecialchars(trim(strip_tags($_REQUEST["password"])));
 
-	$dao = new UsuarioDAO();
-	$usuario = $dao->getUsuarioPorEmail($email);
-
-	#Mostrar por pantalla. No se si así funciona.
-    if(!$usuario){ //si el email no esta en la bd
-        echo "<p>Usuario no registrado</p>";
-        exit();
-    }
-
-    $hash = $usuario->getHashPassword();
-	if (password_verify(hash("md5", $password), $hash)){
-		$_SESSION["login"] = true;
-		$_SESSION["nombre"] = $usuario->getNombre();
-		$_SESSION["idUsuario"] = $usuario->getId();
-		$_SESSION["rol"] = $usuario->getRol();
-		$_SESSION["aprobado"] = $usuario->getAprobado();
-	}
-
-	header("Location: ../perfil.php");
+	if(checkLogin($email, $password))
+        header("Location: ../perfil.php");
+    else
+        echo "<p>Usuario o contraseña incorrectos</p>";
 
