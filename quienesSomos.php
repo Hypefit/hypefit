5f66ecb176c842da1997a69d04b648c759967867
Mario Román Dono <mario.romandono@gmail.com>
2021-04-15 22:43:25 +0200

    crear quienesSomos.php

---
+++ quienesSomos.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/includes/config.php';
+
+$tituloPagina = 'Hypefit | Quiénes somos';
+
+$contenidoPrincipal = "<h1> Quiénes somos </h1>";
+$contenidoPrincipal .= "<p>En construcción...</p>";
+
+require __DIR__ . '/includes/comun/layout.php';
