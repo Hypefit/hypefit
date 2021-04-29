cd6fb1fa3fc6392dbf3c172a887ef82369a354a5
LlucBonet <mbonet03@ucm.es>
2021-04-29 12:20:21 +0200

    primeras pruebas css

--- includes/comun/cabecera.php
+++ includes/comun/cabecera.php
@@ -5,17 +5,18 @@ require_once __DIR__.'/../config.php';
 ?>
 
 <header>
-    <img class="logo" src="<?=RUTA_IMGS.'/logo.png'?>" alt="Hypefit logo">
-	
-	<div class="saludo">
-		
-		<a href="<?=RUTA_APP.'/index.php'?>">Inicio</a>
-		<a href="<?=RUTA_APP.'/rutinas.php'?>">Rutinas</a>
-		<a href="<?=RUTA_APP.'/recetas.php'?>">Recetas</a>
-		<a href="<?=RUTA_APP.'/comunidad.php'?>">Comunidad</a>
-			
-		<?php
-			echo saludo();
-		?>
-	</div>
+	<a href="<?=RUTA_APP.'/index.php'?>" title="Inicio">
+        <img class="logo" src="<?=RUTA_IMGS.'/logo.png'?>" alt="Inicio">
+    </a>
+
+
 </header>
+<nav>
+    <ul>
+      <li><a href="<?=RUTA_APP.'/index.php'?>">Inicio</a></li>
+      <li><a href="<?=RUTA_APP.'/rutinas.php'?>">Rutinas</a></li>
+      <li><a href="<?=RUTA_APP.'/recetas.php'?>">Recetas</a></li>
+      <li><a href="<?=RUTA_APP.'/comunidad.php'?>">Comunidad</a></li>
+    <?php echo saludo(); ?>
+    </ul>
+</nav>
