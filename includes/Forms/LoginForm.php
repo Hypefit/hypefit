10df8f7204ba204135c0bd3023caa59989a16ebd
Mario Román Dono <mario.romandono@gmail.com>
2021-05-03 19:25:13 +0200

    refactorizar login

---
+++ includes/Forms/LoginForm.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace hypefit\Forms;
+
+require_once '../config.php';
+
+class LoginForm extends Form {
+    public function __construct() {
+        parent::__construct('LoginForm');
+    }
+
+    protected function generaCamposFormulario($datosIniciales, $errores = array())  {
+        $email =$datosIniciales['email'] ?? '';
+
+        $htmlErroresGlobales = self::generaListaErroresGlobales($errores);
+
+        $html = <<<EOS
+        <fieldset>
+		<legend>Usuario y contraseña</legend>
+		$htmlErroresGlobales
+			<p><label>Email:</label> <input type="email" name="email" value="$email" /></p> 
+			<p><label>Contraseña:</label> <input type="password" name="password" required/></p>
+			<button type="submit">Entrar</button>
+		</fieldset>
+EOS;
+    return $html;
+    }
+
+    protected function procesaFormulario($datos) {
+        $result = array();
+        $email =$datos['email'] ?? null;
+        $password = $datos['password'] ?? null;
+
+        //TODO: no habría que limpiar con htmlspecialchars?
+
+        $usuario = checkLogin($email, $password);
+        if ( ! $usuario ) {
+            $result[] = "El usuario o el password no coinciden";
+        } else {
+            header("Location: " . RUTA_APP . "/perfil.php");
+        }
+        return $result;
+    }
+}
--- login.php
+++ login.php
@@ -1,27 +1,18 @@
 <?php
+
+use hypefit\Forms\LoginForm;
+
 require_once __DIR__.'/includes/config.php';
 require_once __DIR__.'/includes/autorizacion.php';
 
 $tituloPagina= 'Hypefit | Login';
+$contenidoPrincipal = "<h1>Haz login en Hypefit</h1>";
 
 if (!estaLogado()) {
-    $contenidoPrincipal = <<<EOS
-	<h1>Haz login en Hypefit</h1>
-	<form action="includes/procesarLogin.php" method="POST">
-		<fieldset>
-		<legend>Usuario y contraseña</legend>
-			<p><label>Email:</label> <input type="email" name="email" required /></p>
-			<p><label>Contraseña:</label> <input type="password" name="password" required/></p>
-			<button type="submit">Entrar</button>
-		</fieldset>
-	</form>
-EOS;
+    $form = new LoginForm();
+    $contenidoPrincipal .= $form->gestiona();
 } else {
-    $contenidoPrincipal = <<<EOS
-    <h1>Haz login en Hypefit</h1>
-    <p>¡Ya has iniciado sesión! Puedes acceder a <a href="perfil.php">tu perfil</a> o <a href="logout.php">desconectarte</a>.</p>
-EOS;
-
+    $contenidoPrincipal .=  '<p>¡Ya has iniciado sesión! Puedes acceder a <a href="perfil.php">tu perfil</a> o <a href="logout.php">desconectarte</a>.</p>';
 }
 
 	
