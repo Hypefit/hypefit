295d54e4eeb12debd48dbc72fb8cb19d6dff12ca
LlucBonet <mbonet03@ucm.es>
2021-05-05 11:45:12 +0200

    pruebas css

--- includes/comun/cabecera.php
+++ includes/comun/cabecera.php
@@ -11,16 +11,16 @@ require_once __DIR__.'/../config.php';
 
 <nav>
     <ul>
-      <li>
+        <li>
           <a href="<?=RUTA_APP.'/index.php'?>" title="Inicio">
               <img class="logo" src="<?=RUTA_IMGS.'/logo.png'?>" alt="Inicio">
           </a>
-      </li>
-      <li><a href="<?=RUTA_APP.'/index.php'?>">Inicio</a></li>
-      <li><a href="<?=RUTA_APP.'/rutinas.php'?>">Rutinas</a></li>
-      <li><a href="<?=RUTA_APP.'/recetas.php'?>">Recetas</a></li>
-      <li><a href="<?=RUTA_APP.'/comunidad.php'?>">Comunidad</a></li>
-    <?php echo saludo(); ?>
+        </li>
+        <li><a href="<?=RUTA_APP.'/index.php'?>">Inicio</a></li>
+        <li><a href="<?=RUTA_APP.'/rutinas.php'?>">Rutinas</a></li>
+        <li><a href="<?=RUTA_APP.'/recetas.php'?>">Recetas</a></li>
+        <li><a href="<?=RUTA_APP.'/comunidad.php'?>">Comunidad</a></li>
+        <li><?php echo saludo(); ?></li>
     </ul>
 </nav>
 </header>
