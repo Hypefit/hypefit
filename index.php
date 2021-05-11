6cef905afefa19b1c01ff3e1791806040e1bb00d
LlucBonet <mbonet03@ucm.es>
2021-05-11 15:58:18 +0200

    estilo index acabado

--- index.php
+++ index.php
@@ -20,32 +20,34 @@ $contenidoPrincipal = <<<EOS
                 <div class="row ps-0 justify-content-center">
                     <!--Cards-->
                     <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 pb-2">
-                        <div class="card h-100 border-secondary text-center ">
+                        <div class="card h-100 border-secondary ms-2 text-center ">
                             <a href="rutinas.php">
                                 <img class="card-img-top" src="${rutaImgSup}" alt="Tren Superior" style="height: 17rem;">
                             </a>
                             <div class="card-body">
-                                <h5 class="card-title">Tren superior</h5>
+                                <h5 class="card-title text-capitalize">Tren superior</h5>
                                 <p class="card-text">Ejercicios para fortalecer la parte superior del cuerpo</p>
                             </div>
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 pb-2">
-                        <div class="card h-100 border-secondary text-center">
-                            <img class="card-img-top " src="${rutaImgSup}" alt="Tren Inferior" style="height: 17rem;">
-                        
+                        <div class="card h-100 border-secondary ms-2 text-center">
+                            <a href="rutinas.php">
+                                <img class="card-img-top " src="${rutaImgSup}" alt="Tren Inferior" style="height: 17rem;">
+                            </a>
                             <div class="card-body">
-                                <h5 class="card-title">Tren inferior</h5>
+                                <h5 class="card-title text-capitalize">Tren inferior</h5>
                                 <p class="card-text">Ejercicios para fortalecer la parte inferior del cuerpo</p>
                             </div>
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 pb-2">
-                        <div class="card h-100 border-secondary text-center">
-                            <img class="card-img-top" src="${rutaImgSup}" alt="Full Body" style="height: 17rem;">
-                        
+                        <div class="card h-100 border-secondary ms-2 text-center">
+                            <a href="rutinas.php">
+                              <img class="card-img-top" src="${rutaImgSup}" alt="Full Body" style="height: 17rem;">
+                            </a>
                             <div class="card-body">
-                                <h5 class="card-title">Full Body</h5>
+                                <h5 class="card-title text-capitalize">Full Body</h5>
                                 <p class="card-text">Ejercicios para fortalecer todo el cuerpo en un solo entrenamiento</p>
                             </div>
                         </div>
@@ -64,10 +66,10 @@ $contenidoPrincipal = <<<EOS
     </div>
     <!--Rutinas-->
     
-    <div class="bg-light" style="height: 9%;"></div>
     <!--Recetas-->
-    <div class="container-fluid px-0">
-        <div class="recetas row ps-0 me-0 align-items-center justify-content-center"">
+    <div class="recetas" style="height: 9%;"></div>
+    <div class="recetas container-fluid px-0">
+        <div class=" row ps-0 me-0 align-items-center justify-content-center"">
             <div class="outer container">
                 <!--Rows-->
                 <div class="row text-white justify-content-center align-items-center">
@@ -76,35 +78,35 @@ $contenidoPrincipal = <<<EOS
                 <div class="row ps-0 justify-content-center">
                     <!--Cards-->
                     <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3  pb-2">
-                        <div class="card h-100 border-secondary ms-2 text-center">
+                        <div class="card h-100 border-dark ms-2 text-center">
                             <a href="recetas.php" >
                                 <img style="height: 17rem;" class="card-img-top" src="${rutaImgSup}" alt="sin requerimientos">
                             </a>
                             <div class="card-body">
-                                <h5 class="card-title">Dieta sin requerimientos</h5>
+                                <h5 class="card-title text-capitalize">Dieta sin requerimientos</h5>
                                 <p class="card-text">Recetas sin ningún tipo de restricción en cuanto a los alimentos
                                 a ingerir</p>
                             </div>
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 pb-2">
-                        <div class="card h-100 border-secondary ms-2 text-center">
+                        <div class="card h-100 border-dark ms-2 text-center">
                             <a href="recetas.php">
                                 <img style="height: 17rem;" class="card-img-top " src="${rutaImgSup}" alt="Vegetariana">
                             </a>
                             <div class="card-body">
-                                <h5 class="card-title">Dieta vegetariana</h5>
+                                <h5 class="card-title text-capitalize">Dieta vegetariana</h5>
                                 <p class="card-text">Recetas sin carne ni pesacado</p>
                             </div>
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 pb-2">
-                        <div class="card h-100 border-secondary ms-2 text-center">
+                        <div class="card h-100 border-dark ms-2 text-center">
                             <a href="recetas.php">
                                 <img style="height: 17rem;" class="card-img-top " src="${rutaImgSup}" alt="Vegana">
                             </a>
                             <div class="card-body">
-                                <h5 class="card-title">Dieta vegana</h5>
+                                <h5 class="card-title text-capitalize">Dieta vegana</h5>
                                 <p class="card-text">Recetas sin ningún producto de origen animal</p>
                             </div>
                         </div>
@@ -124,11 +126,16 @@ $contenidoPrincipal = <<<EOS
     </div>
     
     <!--Calendario-->
-    <div class="calendario">
-        <h4> CALENDARIO ACTIVIDADES</h4>
-        <img src="img/calendario-abril-2021.jpg" alt="Imagen calendario provisional">
+    <div class="bg-light" style="height: 9%;"></div>
+    <div class="container-fluid px-0">
+        <div class="bg-light row ps-0 me-0 align-items-center justify-content-center">
+            <h2 class="text-uppercase mt-3 mb-4 text-center font-weight-bold">Calendario de actividades</h2>
+             <img src="img/calendario-abril-2021.jpg" alt="Imagen calendario provisional" style="width: 50%;">
+        </div>
     </div>
+    <div class="bg-light" style="height: 9%;"></div>
     <!--Calendario-->
+    
     <!--<div class="quienesSomos">
         <h4>¿QUIÉNES SOMOS?</h4>
         <p>Hypefit es un innovador gimnasio online que incluye tanto recetas como rutinas equilibradas y pensadas para
--- recetas.php
+++ recetas.php
@@ -4,11 +4,11 @@ require_once __DIR__ . '/includes/funcionesRecetas.php';
 $tituloPagina = 'Hypefit | Recetas';
 
 $contenidoPrincipal = "<h1> RECETAS </h1>";
-$contenidoPrincipal .= "<h2> ------------------- WITHOUT REQUIREMENTS ------------------- </h2>";
+$contenidoPrincipal .= "<h2> ------------------- DIETA SIN REQUERIMIENTOS ------------------- </h2>";
 $contenidoPrincipal .= crearListaRecetas("normal");
-$contenidoPrincipal .= "<h2> ------------------- VEGETARIAN DIET ------------------- </h2>";
+$contenidoPrincipal .= "<h2> ------------------- DIETA VEGETARIANA ------------------- </h2>";
 $contenidoPrincipal .= crearListaRecetas("vegetariana");
-$contenidoPrincipal .= "<h2> ------------------- VEGAN DIET ------------------- </h2>";
+$contenidoPrincipal .= "<h2> ------------------- DIETA VEGANA ------------------- </h2>";
 $contenidoPrincipal .= crearListaRecetas("vegana");
 $contenidoPrincipal .= "<h3><a href='crearReceta.php'>Crea una nueva receta</a></h3>";
 $contenidoPrincipal .= "</div>";
