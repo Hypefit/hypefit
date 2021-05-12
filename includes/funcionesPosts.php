08f189b191cfcda66cbd21891a4f941d565cdde8
sanduerhan <61199730+sanduerhan@users.noreply.github.com>
2021-05-12 20:27:32 +0200

    Update funcionesPosts.php

--- includes/funcionesPosts.php
+++ includes/funcionesPosts.php
@@ -12,7 +12,16 @@ function crearListaPosts(): string {
 
     $html = "<ul>";
     foreach($lista as $post) {
-        $html .= "<li> <a href='verPost.php?id=" . $post->getId() . "'>" . $post->getTitulo() ."</a> </li>";
+        $html .= "
+            <div class='col-xs-12 col-sm-12 col-md-12 col-lg-12 pb-12'>
+                <div class='card h-100 border-secondary ms-2 text-center'>
+                    <h5 class='card-header text-capitalize'>" . $post->getTitulo() . "</h5>
+                    <div class='card-body'>
+                        <a href='verPost.php?id=" . $post->getId() . "'' class='btn btn-primary'>Ver Post</a>
+                    </div>        
+                </div>
+            </div>
+            ";
     }
     $html .= "</ul>";
 
@@ -26,12 +35,12 @@ function mostrarPost($id) : string {
     if ($post == null) {
         return "<p>No existe ningún post con el id especificado.</p>";
     } else {
-        $dao = new UsuarioDAO();
-        $creador = $dao->getUsuario($post->getIdCreador());
-        $nombreCreador = $creador->getNombre();
+        //$dao = new UsuarioDAO();
+        //$creador = $dao->getUsuario($post->getIdCreador());
+        //$nombreCreador = $creador->getNombre();
 
         $html = "<h1>" . $post->getTitulo() . "</h1>";
-        $html .= "Creado por: " . $nombreCreador . "<br>";
+        //$html .= "Creado por: " . $nombreCreador . "<br>";
         $html .= mostrarComentariosPost($id);
 
         return $html;
@@ -47,8 +56,31 @@ function mostrarComentariosPost($id) : string {
     $html = "<ul>";
     foreach($comentarios as $comentario) {
         $user = $daoU->getUsuario($comentario->getIdUsuario());
-        $html .= "<li>" . $user->getNombre() . " | " .  $comentario->getFecha() . "<br>" .
         $comentario->getComentario() ."</li>";
+        //$html .= "<li>" . $user->getNombre() . " | " .  $comentario->getFecha() . "<br>" .
+        $fecha = $comentario->getFecha();
+        $texto = $comentario->getComentario();
+        $username = $user->getNombre();
+        
+        $html .= <<<EOS
+<div class="container-fluid mt-100">
+<div class="row">
+    <div class="col-md-12">
+        <div class="card mb-4">
+            <div class="card-header">  
+                    <div class="media-body ml-3">"$username"</a>
+                        <div class="text-muted small">"$fecha"</div>
+                    </div>  
+            </div>
+            <div class="card-body">
+                <p>"$texto"</p>
+            </div>
+            
+        </div>
+    </div>
+</div>
+</div>
+EOS;
     }
     $html .= "</ul>";
 
