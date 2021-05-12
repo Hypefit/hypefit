a586bf0286e55c25a7c57850ac28a6e1ccaae315
sanduerhan <61199730+sanduerhan@users.noreply.github.com>
2021-05-12 20:29:34 +0200

    Update CrearComentarioForm.php

--- includes/Forms/CrearComentarioForm.php
+++ includes/Forms/CrearComentarioForm.php
@@ -20,12 +20,35 @@ class CrearComentarioForm extends Form {
 
         $idPost = htmlspecialchars(trim(strip_tags($_REQUEST["id"])));
         $html = <<<EOS
-        <fieldset>
-            $htmlErroresGlobales
-            <input type="hidden" name="idPost" value="$idPost" />
-				<div><label for="mensaje">Respuesta: </label><input id="mensaje" type="text" name="mensaje" /></div>
-				<div><button type="submit">Crear</button></div>
-        </fieldset>
+<!-- Jumbotron -->
+    <div class="p-5 text-center bg-image img-fluid"
+        style="
+            background-image: url(https://s1.1zoom.me/b5249/490/Closeup_15_kg_Barbell_514746_1366x768.jpg);
+            opacity: 0.9;
+            background-repeat: no-repeat;
+            background-size: cover;
+            width:  auto;
+            height: 100%;
+            margin: 5%;
+        ">
+        <div class="mask" style="background-color: rgba(255, 255, 255, 0.7); margin: 10%;">
+            <div class="d-flex justify-content-center align-items-center h-100">
+                <div class="text-black">
+                    <fieldset>
+                        <legend class="mt-4 mb-3">Escribe tu respuesta</legend>
+                        <div class="mb-3">
+                        <input type="hidden" name="idPost" value="$idPost" />
+                        <div><label for="mensaje">Respuesta: </label><input id="mensaje" type="text" name="mensaje" /></div>
+                        </div>
+                        $htmlErroresGlobales
+                        <button type="submit" class="btn btn-dark">Crear</button>
+                        <h5></h5>
+                        <p></p>
+                    </fieldset>
+                </div>
+            </div>
+        </div>
+    </div>
 EOS;
         return $html;
     }
