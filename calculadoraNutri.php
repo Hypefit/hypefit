d29f6624190f9a8bcf5cd830269b47fc13bfa272
AlvaroPlaza <57074350+AlvaroPlaza@users.noreply.github.com>
2021-05-12 11:28:32 +0200

    ComentarioRecetaDAO.php y ComentarioRutinaDAO.php

--- calculadoraNutri.php
+++ calculadoraNutri.php
@@ -11,22 +11,44 @@ $contenidoPrincipal = <<<EOS
     <h1>Utiliza nuestra calculadora nutricional para saber si has logrado tus objetivos</h1>
     <form action="includes/procesarNutri.php" method="POST">
         <fieldset>
-            <legend>Rellena los datos sobre tu rutina y alimentación:</legend>
-            <p><label>Peso actual:</label> <input type="text" name="pesoA" required/></p>
-            <p><label>Peso objetivo:</label> <input type="text" name="pesoO" required/></p>
+            <legend>Rellena los datos sobre tu alimentación:</legend>
             <p><label>¿Cuál es tu objetivo calórico?</label></p> <select name="objetivo" required>
                 <option value="Déficit Calórico" selected> Déficit Calórico</option>
                 <option value="Equilibrio Calórico">Equilibrio Calórico</option>
                 <option value="Superávit Calórico">Superávit Calórico</option>
             </select>
             
-            <p><label>Calorías:</label> <input type="number" name="calorias" required/></p>
+            <h3>Comida número 1:</h3>
+            <p><label>Calorías :</label> <input type="number" name="calorias" required/></p>
             <p><label>Proteínas:</label> <input type="number" name="proteinas" required/></p>
             <p><label>Grasas:</label> <input type="number" name="grasas" required/></p>
             <p><label>Carbohidratos:</label> <input type="number" name="carbos" required/></p>
-            <p><label>Ejercicio realizado:</label> <input type="text" name="ejercicio" required/></p>
-            <p><label>Tiempo de ejercicio realizado(minutos):</label> <input type="number" name="tiempo" required/></p>
-
+          
+             <h3>Comida número 2</h3>
+            <p><label>Calorías :</label> <input type="number" name="calorias2" required/></p>
+            <p><label>Proteínas:</label> <input type="number" name="proteinas2" required/></p>
+            <p><label>Grasas:</label> <input type="number" name="grasas2" required/></p>
+            <p><label>Carbohidratos:</label> <input type="number" name="carbos2" required/></p>
+            
+               <h3>Comida número 3</h3>
+            <p><label>Calorías :</label> <input type="number" name="calorias3" required/></p>
+            <p><label>Proteínas:</label> <input type="number" name="proteinas3" required/></p>
+            <p><label>Grasas:</label> <input type="number" name="grasas3" required/></p>
+            <p><label>Carbohidratos:</label> <input type="number" name="carbos3" required/></p>
+            
+               <h3>Comida número 4</h3>
+            <p><label>Calorías :</label> <input type="number" name="calorias4" required/></p>
+            <p><label>Proteínas:</label> <input type="number" name="proteinas4" required/></p>
+            <p><label>Grasas:</label> <input type="number" name="grasas4" required/></p>
+            <p><label>Carbohidratos:</label> <input type="number" name="carbos4" required/></p>
+            
+            
+               <h3>Comida número 5</h3>
+            <p><label>Calorías :</label> <input type="number" name="calorias5" required/></p>
+            <p><label>Proteínas:</label> <input type="number" name="proteinas5" required/></p>
+            <p><label>Grasas:</label> <input type="number" name="grasas5" required/></p>
+            <p><label>Carbohidratos:</label> <input type="number" name="carbos5" required/></p>
+          
             <button type="Calcular">Calcular</button>
         </fieldset>
     </form>
