124ece52872571781260fa030960fc0303b41fcf
LlucBonet <mbonet03@ucm.es>
2021-06-02 13:26:16 +0200

    estilo mensaje respuesta comentario

--- includes/Forms/CrearComentarioForm.php
+++ includes/Forms/CrearComentarioForm.php
@@ -12,10 +12,12 @@ class CrearComentarioForm extends Form {
 
     private $idPost;
     private $idComentarioPadre;
+    private $es_Respuesta;
 
-    public function __construct($idPost, $idComentarioPadre) {
+    public function __construct($idPost, $idComentarioPadre, $es_Respuesta = false) {
         $this->idPost = $idPost;
         $this->idComentarioPadre = $idComentarioPadre;
+        $this->es_Respuesta = $es_Respuesta;
         $opciones = array( 'action' => RUTA_APP . "/verPost.php?id=" . $idPost );
         parent::__construct('CrearComentarioForm', $opciones);
     }
@@ -23,28 +25,44 @@ class CrearComentarioForm extends Form {
     protected function generaCamposFormulario($datosIniciales, $errores = array())  {
         $htmlErroresGlobales = self::generaListaErroresGlobales($errores);
 
-        $html = <<<EOS
-        <!--Caja de respuesta-->
-        <input type="hidden" name="idPost" value="$this->idPost">
-        <input type="hidden" name="idComentarioPadre" value="$this->idComentarioPadre">
-        <div class='container-fluid p-4'>
-            <div class='row justify-content-center align-items-center justify-content-center'>
-                <div class="col-sm-8 mask border rounded shadow m-5" style="background-color: rgba(255, 255, 255, 0.7);">
-                    <div class=" text-center">
-                        <legend class="mt-4 mb-3">Escribe tu respuesta</legend>
-                        <div class="mb-3 row justify-content-center">
-                            <div class="col col-sm-8">
-                                <textarea  title="Mensaje" class="form-control" name="mensaje" 
-                                    placeholder="Mensaje" required id="mensaje"></textarea>
+        if($this->es_Respuesta){
+            $html = <<<EOS
+                <input type="hidden" name="idPost" value="$this->idPost">
+                <input type="hidden" name="idComentarioPadre" value="$this->idComentarioPadre">
+                <div class = "mb-3 ms-5">
+                    <div class = "mb-2 w-75">
+                        <textarea title="RespuestaMensaje" class="form-control" name="respuestaMensaje" 
+                        placeholder="Deja tu respuesta..." required id="respuestaMensaje"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-dark">Comentar</button>
+                </div>  
+            EOS;
+        }
+
+        else {
+            $html = <<<EOS
+            <!--Caja de respuesta-->
+            <input type="hidden" name="idPost" value="$this->idPost">
+            <input type="hidden" name="idComentarioPadre" value="$this->idComentarioPadre">
+            <div class='container-fluid p-4'>
+                <div class='row justify-content-center align-items-center justify-content-center'>
+                    <div class="col-sm-8 mask border rounded shadow m-5" style="background-color: rgba(255, 255, 255, 0.7);">
+                        <div class=" text-center">
+                            <legend class="mt-4 mb-3">Escribe tu respuesta</legend>
+                            <div class="mb-3 row justify-content-center">
+                                <div class="col col-sm-8">
+                                    <textarea  title="Mensaje" class="form-control" name="mensaje" 
+                                        placeholder="Mensaje" required id="mensaje"></textarea>
+                                </div>
                             </div>
+                            $htmlErroresGlobales
+                            <button type="submit" class="btn btn-dark mb-3">Crear</button>
                         </div>
-                        $htmlErroresGlobales
-                        <button type="submit" class="btn btn-dark mb-3">Crear</button>
                     </div>
                 </div>
             </div>
-        </div>
-        EOS;
+            EOS;
+        }
         return $html;
     }
 
