2cdbfcf8fcda36df35512db05014ef4b7ceffc5f
Mario Román Dono <mario.romandono@gmail.com>
2021-06-04 14:08:43 +0200

    Utilizar CDNs para las dependencias

--- includes/comun/layout.php
+++ includes/comun/layout.php
@@ -14,14 +14,18 @@
         <meta http-equiv="Pragma" content="no-cache">
 
     <!--Bootstrap-->
-    <link rel="stylesheet" type="text/css" href="<?= RUTA_CSS.'/bootstrap.css'?>" />
+    <!--<link rel="stylesheet" type="text/css" href="<?= RUTA_CSS.'/bootstrap.css'?>" />-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
     <!--CSS-->
     <link rel="stylesheet" type="text/css" href="<?= RUTA_CSS.'/estilo.css'?>" />
 
     <!--jQuery UI-->
-    <link rel="stylesheet" type="text/css" href="<?= RUTA_CSS.'/jquery-ui.min.css'?>" />
+    <!--<link rel="stylesheet" type="text/css" href="<?= RUTA_CSS.'/jquery-ui.min.css'?>" />-->
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 
+    <!--TempusDominus-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.39.0/css/tempusdominus-bootstrap-4.min.css" integrity="sha512-3JRrEUwaCkFUBLK1N8HehwQgu8e23jTH4np5NHOmQOobuC4ROQxFwFgBLTnhcnQRMs84muMh0PnnwXlPq5MGjg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!--FullCalendar-->
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@fullcalendar/core@4.4.2/main.min.css"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@4.4.2/main.min.css"></script>
@@ -30,31 +34,11 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@fullcalendar/bootstrap@4.4.2/main.min.css"></script>
 
 
-    <!--Bootstrap-->
-    <script src="<?= RUTA_JS.'/bootstrap.bundle.js'?>"></script>
-
-    <!--jQuery-->
-    <script src="<?= RUTA_JS.'/jquery-3.6.0.min.js'?>"></script>
-
-    <!--jQuery UI-->
-    <script src="<?= RUTA_JS.'/jquery-ui.min.js'?>"></script>
-
-    <!--FullCalendar-->
-    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/core@4.4.2/main.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/interaction@4.4.2/main.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@4.4.2/main.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/timegrid@4.4.2/main.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/list@4.4.2/main.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/bootstrap@4.4.2/main.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/core@4.4.2/locales/es.js"></script>
-
     <!--Favicon-->
     <link rel="icon"
           type="image/png"
           href="<?= RUTA_IMGS.'/favicon.png'?>"/>
 
-    <!--Font Awesome (iconos chulos)-->
-    <script src="https://kit.fontawesome.com/c4984e3c67.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div id="contenedor">
@@ -71,9 +55,35 @@
         ?>
     </div>
 
-    <!--JS-->
-    <script src="<?= RUTA_JS.'/validationRegistro.js'?>"></script>
-    <script src="<?= RUTA_JS.'/validationLogin.js'?>"></script>
+    <!--jQuery-->
+    <!--<script src="<?= RUTA_JS.'/jquery-3.6.0.min.js'?>"></script>-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <!--jQuery UI-->
+    <!--<script src="<?= RUTA_JS.'/jquery-ui.min.js'?>"></script>-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+    <!--Moment.js-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment-with-locales.min.js" integrity="sha512-LGXaggshOkD/at6PFNcp2V2unf9LzFq6LE+sChH7ceMTDP0g2kn6Vxwgg7wkPP7AAtX+lmPqPdxB47A0Nz0cMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!--Bootstrap-->
+    <!--<script src="<?= RUTA_JS.'/bootstrap.bundle.js'?>"></script>-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!--Font Awesome (iconos chulos)-->
+    <script src="https://kit.fontawesome.com/c4984e3c67.js" crossorigin="anonymous"></script>
+
+    <!--TempusDominus-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.39.0/js/tempusdominus-bootstrap-4.min.js" integrity="sha512-k6/Bkb8Fxf/c1Tkyl39yJwcOZ1P4cRrJu77p83zJjN2Z55prbFHxPs9vN7q3l3+tSMGPDdoH51AEU8Vgo1cgAA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!--FullCalendar-->
+    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/core@4.4.2/main.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/interaction@4.4.2/main.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@4.4.2/main.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/timegrid@4.4.2/main.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/list@4.4.2/main.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/bootstrap@4.4.2/main.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fullcalendar/core@4.4.2/locales/es.js"></script>
 
     <?php
         if(isset($scripts)) {
